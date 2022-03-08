8c8fa0ebd1ed6139a85b88fe71d9fab0480538aa
Jul Guga <mr.guga89@hotmail.com>
2022-03-08 10:22:45 +0100

    solved data visualization

--- src/components/Characters/index.tsx
+++ src/components/Characters/index.tsx
@@ -1,3 +1,4 @@
+import CircularProgress from "@mui/material/CircularProgress";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -11,7 +12,7 @@ import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { charactersSelector, getCharacters } from "./charactersSlice";
 
 interface Column {
-  id: "name" | "code" | "population" | "size" | "density";
+  id: "char_id" | "name" | "birthday" | "occupation" | "status";
   label: string;
   minWidth?: number;
   align?: "right";
@@ -19,65 +20,24 @@ interface Column {
 }
 
 const columns: Column[] = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+  { id: "char_id", label: "ID", minWidth: 5 },
+  { id: "name", label: "Name", minWidth: 100 },
   {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
+    id: "birthday",
+    label: "Birthday",
+    minWidth: 100,
     format: (value: number) => value.toLocaleString("en-US"),
   },
   {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
+    id: "occupation",
+    label: "Occupation",
     minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
   },
   {
-    id: "density",
-    label: "Density",
+    id: "status",
+    label: "Status",
     minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toFixed(2),
   },
-];
-
-interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
-}
-
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number
-): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
 ];
 
 interface ICharactersProps {}
@@ -85,7 +45,6 @@ interface ICharactersProps {}
 const Characters: React.FC<ICharactersProps> = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
-
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -105,56 +64,65 @@ const Characters: React.FC<ICharactersProps> = () => {
     setPage(0);
   };
 
-  return (
-    <Paper sx={{ width: "100%" }}>
-      <TableContainer sx={{ maxHeight: 570 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 20, 40]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
-  );
+  if (loading) {
+    return <CircularProgress />;
+  } else {
+    return (
+      <Paper sx={{ width: "100%" }}>
+        <TableContainer sx={{ maxHeight: 570 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {response
+                ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((character: any) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={character.char_id}
+                    >
+                      {columns.map((column) => {
+                        const value = character[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === "number"
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 20, 40]}
+          component="div"
+          count={response?.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    );
+  }
 };
 
 export default Characters;
